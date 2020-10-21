1b93f535efc63dbf4d5d25b020ec560dc158fcd0
Nhan Pham <n2pham@ucsd.edu>
2020-10-20 23:03:07 -0700

    updated config

--- TritonRacerSim/core/config.py
+++ TritonRacerSim/core/config.py
@@ -3,10 +3,13 @@ import uuid
 config = {
     'explanation': '''model_type: cnn_2d | cnn_2d_speed_as_feature | cnn_2d_speed_control | cnn_2d_full_house; joystick_type: ps4 | xbox | g28; sim_host: use 127.0.0.1 for local; track_data_file: used for position tracker to segment the track
     ''',
-    # Camera
-    'img_w': 160,
-    'img_h': 120,
+    # Camera Type
     'cam_type': 'WEBCAM', # WEBCAM | MOCK Put mock if the camera is not actually installed
+    'img_w': 160, # Width after cropping
+    'img_h': 120, # Height after cropping
+    'image_format': 'rgb', # RGB or grey
+    'cam_source': 0, # Index of camera
+    'cam_resolution': [320, 240], # Resolution of open camera, check with manufacturer for resolution support
 
     # Image Preprocessing
     'preprocessing_enabled': False, # Enable image filtering
